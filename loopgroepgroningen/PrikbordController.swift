8060b26b4ac548b6f0f664ff0ebfba1f78658bce
Michel Vollebregt <mi.vollebregt@gmail.com>
2017-06-17 14:38:26 +0200

    Rounded corners

--- loopgroepgroningen/PrikbordController.swift
+++ loopgroepgroningen/PrikbordController.swift
@@ -53,6 +53,9 @@ class PrikbordController: UITableViewController {
             cell.auteurLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         }
 //        cell.berichtTextView.layoutIfNeeded()
+        
+        cell.contentView.layer.cornerRadius = 30.0;
+        cell.contentView.layer.masksToBounds = true
 
         return cell
     }
